532d7ecbc078a2b7815422aba5c4ba74fdbce5af
Pasupuleti Rudrama Naidu <121972044+rudrasgithub@users.noreply.github.com>
2025-12-12 10:19:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 
 const divide = (arr, n) => {
-	let sum = 0;ans.append()
+	let sum = 0;
 	let ans = [];
 	let chunk_arr = []
 	for(let i=0;i<arr.length;i++) {
